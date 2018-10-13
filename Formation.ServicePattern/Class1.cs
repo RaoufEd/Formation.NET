20e9ef5a143ee6d2e3956120eb0b67c1a61ee45e
RaoufEd <raouf.edabbar@gmail.com>
2018-10-13 15:07:04 +0100

    Add project files.

---
+++ Formation.ServicePattern/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation.ServicePattern
+{
+    public class Class1
+    {
+    }
+}
